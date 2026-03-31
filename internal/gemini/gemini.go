da656d4e8cb5fdd6b4d2b8818cda36909a515bcf
tungtn <tungtn1099@gmail.com>
2026-03-31 14:58:29 +0700

    switch model

--- internal/gemini/gemini.go
+++ internal/gemini/gemini.go
@@ -10,7 +10,7 @@ import (
 )
 
 const (
-	GeminiAPIURL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+	GeminiAPIURL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent"
 )
 
 type ContentPart struct {
